93581156fd689d8d4d32fcd8894acc60a81fd473
dhavalvasveliya <vasveliyadhaval04@gmail.com>
2022-11-18 17:58:18 -0500

    Files Added

---
+++ main.py
@@ -0,0 +1,20 @@
+def odds_sum(L):
+    """Return the sum of the odd elements of L."""
+    return sum([i for i in L if i % 2 != 0])
+
+def has_letter_cases(s: str):
+    """
+    Return True if and only if s contains at least one lowercase letter and at 
+    least one uppercase letter.
+    """
+    return not s.isupper() and not s.islower()
+
+def find_lowercase_vowel(msg: str):
+    """
+    Return the index of the first lowercase vowel (a, e, i, o, u) in msg, 
+    or the length of msg if it does not contain any lowercase vowels.
+    """
+    for index, char in enumerate(msg):
+        if char in 'aeiou':
+            return index
+    return len(msg)
---
+++ test.py
@@ -0,0 +1,17 @@
+#importing our app.py file so that we can use functions
+from main import odds_sum,has_letter_cases,find_lowercase_vowel
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_odds_sum(self):
+        self.assertEqual(odds_sum([1, 2, 3]),4, "Should be 6")
+
+    def test_has_letter_cases(self):
+        self.assertEqual(has_letter_cases("abcDEF"),True, "Should be True")
+
+    def test_find_lowercase_vowel(self):
+        self.assertEqual(find_lowercase_vowel("cats"),1, "Should be 1")
+
+if __name__ == '__main__':
+    unittest.main()
+
